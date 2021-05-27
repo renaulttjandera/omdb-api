5a5f769441d7c7539470875c737539215f6c223c
Renault Tjandera <80615869+renaulttjandera@users.noreply.github.com>
2021-05-27 19:46:09 +0700

    changing url

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function searchMovie() {
   $("#movie-list").html("");
   $.ajax({
-    url: "http://omdbapi.com",
+    url: "https://omdbapi.com",
     type: "get",
     dataType: "json",
     data: {
@@ -61,7 +61,7 @@ $("#search-input").on("keyup", function (e) {
 
 $("#movie-list").on("click", ".see-details", function () {
   $.ajax({
-    url: "http://omdbapi.com",
+    url: "https://omdbapi.com",
     type: "get",
     dataType: "json",
     data: {
